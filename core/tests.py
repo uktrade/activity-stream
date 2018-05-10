c50377093aa27f27ad495760506fc731e8fb8ed1
Michal Charemza <michal@charemza.name>
2018-05-10 16:44:21 +0100

    (style) Indentation is 4 spaces (PEP8)

--- core/tests.py
+++ core/tests.py
@@ -6,16 +6,16 @@ import urllib.request
 class TestServer(unittest.TestCase):
 
     def setUp(self):
-      self.server = Popen(["gunicorn", "conf.wsgi", "--config", "conf/gunicorn.py"])
+        self.server = Popen(['gunicorn', 'conf.wsgi', '--config', 'conf/gunicorn.py'])
 
     def tearDown(self):
-      self.server.kill()
+        self.server.kill()
 
     def test_server_accepts_http(self):
-      def is_http_accepted():
-          try:
-            urllib.request.urlopen('http://localhost:8000', timeout=1)
-            return True
-          except urllib.request.URLError as e:
-            return 'nodename nor servname provided, or not known' not in str(e.reason)
-      self.assertTrue(is_http_accepted())
+        def is_http_accepted():
+            try:
+                urllib.request.urlopen('http://localhost:8000', timeout=1)
+                return True
+            except urllib.request.URLError as e:
+                return 'nodename nor servname provided, or not known' not in str(e.reason)
+        self.assertTrue(is_http_accepted())
