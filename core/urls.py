3f5a4f5c2502e0be703158f7f4a77c2dc30fe146
Michal Charemza <michal@charemza.name>
2018-05-10 15:03:50 +0100

    (feat) Remove database and django
    Keeping a basic webserver so it does _something_, and it's likely we will want
    to expose a single endpoint.

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('actions/<str:action_type_name>/', views.add_action),
-    path('actions/<int:action_id>', views.get_action_by_id),
-]
