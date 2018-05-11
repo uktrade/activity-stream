5536a57515a3bb2f597ba5c3a9408d217f083076
Michal Charemza <michal@charemza.name>
2018-05-11 12:11:29 +0100

    (refactor) Separate to functions

--- core/app.py
+++ core/app.py
@@ -4,16 +4,22 @@ import sys
 from aiohttp import web
 
 
-async def handle(request):
-    return web.Response(text='')
+def run_application():
+    async def handle(request):
+        return web.Response(text='')
 
-app = web.Application()
-app.add_routes([web.get('/', handle)])
-web.run_app(app,
-            access_log_format='%a %t "%r" %s %b "%{Referer}i" "%{User-Agent}i" %{X-Forwarded-For}i'
-            )
+    app = web.Application()
+    app.add_routes([web.get('/', handle)])
+    access_log_format = '%a %t "%r" %s %b "%{Referer}i" "%{User-Agent}i" %{X-Forwarded-For}i'
+    web.run_app(app, access_log_format=access_log_format)
 
-stdout_handler = logging.StreamHandler(sys.stdout)
-aiohttp_log = logging.getLogger('aiohttp.access')
-aiohttp_log.setLevel(logging.DEBUG)
-aiohttp_log.addHandler(stdout_handler)
+
+def setup_logging():
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    aiohttp_log = logging.getLogger('aiohttp.access')
+    aiohttp_log.setLevel(logging.DEBUG)
+    aiohttp_log.addHandler(stdout_handler)
+
+
+setup_logging()
+run_application()
