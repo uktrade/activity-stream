ca2c4e5b409b12c270ec8a4c07ef9dfd8b79573c
Michal Charemza <michal@charemza.name>
2018-06-12 13:36:31 +0100

    (refactor) Snake_case is more usual

--- core/app.py
+++ core/app.py
@@ -161,7 +161,7 @@ def es_bulk_auth_headers(access_key, secret_key, region, host, path, payload):
     def sign(key, msg):
         return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
 
-    def getSignatureKey(key, dateStamp, regionName, serviceName):
+    def signature_key(key, dateStamp, regionName, serviceName):
         kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
         kRegion = sign(kDate, regionName)
         kService = sign(kRegion, serviceName)
@@ -185,7 +185,7 @@ def es_bulk_auth_headers(access_key, secret_key, region, host, path, payload):
     credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
     string_to_sign = algorithm + '\n' + amzdate + '\n' + credential_scope + \
         '\n' + hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
-    signing_key = getSignatureKey(secret_key, datestamp, region, service)
+    signing_key = signature_key(secret_key, datestamp, region, service)
     signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
     authorization_header = (algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope +
                             ', ' + 'SignedHeaders=' + signed_headers +
