ad8f8d89d21f067cdcb35a69c99e007f1710dba5
Michal Charemza <michal@charemza.name>
2018-07-02 09:32:35 +0100

    (refactor) Move elasticsearch related function to separate module

--- core/app.py
+++ core/app.py
@@ -1,8 +1,5 @@
 import asyncio
-import datetime
 import functools
-import hashlib
-import hmac
 import json
 import logging
 import os
@@ -12,6 +9,9 @@ import sys
 import aiohttp
 from aiohttp import web
 
+from .app_elasticsearch import (
+    es_auth_headers,
+)
 from .app_feeds import (
     ElasticsearchBulkFeed,
     ZendeskFeed,
@@ -250,51 +250,6 @@ def es_bulk(items):
          json.dumps(item['source'], sort_keys=True)]
         for item in items
     ])) + '\n'
-
-
-def es_auth_headers(endpoint, method, path, payload):
-    service = 'es'
-    signed_headers = 'content-type;host;x-amz-date'
-    algorithm = 'AWS4-HMAC-SHA256'
-
-    now = datetime.datetime.utcnow()
-    amzdate = now.strftime('%Y%m%dT%H%M%SZ')
-    datestamp = now.strftime('%Y%m%d')
-
-    credential_scope = f'{datestamp}/{endpoint["region"]}/{service}/aws4_request'
-
-    def signature():
-        def canonical_request():
-            canonical_uri = path
-            canonical_querystring = ''
-            canonical_headers = \
-                f'content-type:application/x-ndjson\n' + \
-                f'host:{endpoint["host"]}\nx-amz-date:{amzdate}\n'
-            payload_hash = hashlib.sha256(payload).hexdigest()
-
-            return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
-                   f'{canonical_headers}\n{signed_headers}\n{payload_hash}'
-
-        def sign(key, msg):
-            return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
-
-        string_to_sign = \
-            f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
-            hashlib.sha256(canonical_request().encode('utf-8')).hexdigest()
-
-        date_key = sign(('AWS4' + endpoint['secret_key']).encode('utf-8'), datestamp)
-        region_key = sign(date_key, endpoint['region'])
-        service_key = sign(region_key, service)
-        request_key = sign(service_key, 'aws4_request')
-        return sign(request_key, string_to_sign).hex()
-
-    return {
-        'x-amz-date': amzdate,
-        'Authorization': (
-            f'{algorithm} Credential={endpoint["access_key_id"]}/{credential_scope}, ' +
-            f'SignedHeaders={signed_headers}, Signature=' + signature()
-        ),
-    }
 
 
 def setup_logging():
