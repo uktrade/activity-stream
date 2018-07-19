60e024f1db1db41224837ada5a3a8ae8b4a14a39
Michal Charemza <michal@charemza.name>
2018-07-19 06:15:39 +0100

    (refactor) Move lower lever to bottom of file

--- core/app_elasticsearch.py
+++ core/app_elasticsearch.py
@@ -71,51 +71,6 @@ async def ensure_mappings(session, es_endpoint):
         raise Exception(await results.text())
 
 
-def es_auth_headers(endpoint, method, path, query_string, content_type, payload):
-    service = 'es'
-    signed_headers = 'content-type;host;x-amz-date'
-    algorithm = 'AWS4-HMAC-SHA256'
-
-    now = datetime.datetime.utcnow()
-    amzdate = now.strftime('%Y%m%dT%H%M%SZ')
-    datestamp = now.strftime('%Y%m%d')
-
-    credential_scope = f'{datestamp}/{endpoint["region"]}/{service}/aws4_request'
-
-    def signature():
-        def canonical_request():
-            canonical_uri = path
-            canonical_querystring = query_string
-            canonical_headers = \
-                f'content-type:{content_type}\n' + \
-                f'host:{endpoint["host"]}\nx-amz-date:{amzdate}\n'
-            payload_hash = hashlib.sha256(payload).hexdigest()
-
-            return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
-                   f'{canonical_headers}\n{signed_headers}\n{payload_hash}'
-
-        def sign(key, msg):
-            return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
-
-        string_to_sign = \
-            f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
-            hashlib.sha256(canonical_request().encode('utf-8')).hexdigest()
-
-        date_key = sign(('AWS4' + endpoint['secret_key']).encode('utf-8'), datestamp)
-        region_key = sign(date_key, endpoint['region'])
-        service_key = sign(region_key, service)
-        request_key = sign(service_key, 'aws4_request')
-        return sign(request_key, string_to_sign).hex()
-
-    return {
-        'x-amz-date': amzdate,
-        'Authorization': (
-            f'{algorithm} Credential={endpoint["access_key_id"]}/{credential_scope}, ' +
-            f'SignedHeaders={signed_headers}, Signature=' + signature()
-        ),
-    }
-
-
 def es_search_new_scroll(_, __, query):
     return '/activities/_search', 'scroll=30s', query
 
@@ -218,3 +173,48 @@ async def es_bulk(session, es_endpoint, items):
         url, data=es_bulk_contents, headers={**headers, **auth_headers})
 
     app_logger.debug('Pushing to ES: done (%s)', await es_result.content.read())
+
+
+def es_auth_headers(endpoint, method, path, query_string, content_type, payload):
+    service = 'es'
+    signed_headers = 'content-type;host;x-amz-date'
+    algorithm = 'AWS4-HMAC-SHA256'
+
+    now = datetime.datetime.utcnow()
+    amzdate = now.strftime('%Y%m%dT%H%M%SZ')
+    datestamp = now.strftime('%Y%m%d')
+
+    credential_scope = f'{datestamp}/{endpoint["region"]}/{service}/aws4_request'
+
+    def signature():
+        def canonical_request():
+            canonical_uri = path
+            canonical_querystring = query_string
+            canonical_headers = \
+                f'content-type:{content_type}\n' + \
+                f'host:{endpoint["host"]}\nx-amz-date:{amzdate}\n'
+            payload_hash = hashlib.sha256(payload).hexdigest()
+
+            return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
+                   f'{canonical_headers}\n{signed_headers}\n{payload_hash}'
+
+        def sign(key, msg):
+            return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+        string_to_sign = \
+            f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
+            hashlib.sha256(canonical_request().encode('utf-8')).hexdigest()
+
+        date_key = sign(('AWS4' + endpoint['secret_key']).encode('utf-8'), datestamp)
+        region_key = sign(date_key, endpoint['region'])
+        service_key = sign(region_key, service)
+        request_key = sign(service_key, 'aws4_request')
+        return sign(request_key, string_to_sign).hex()
+
+    return {
+        'x-amz-date': amzdate,
+        'Authorization': (
+            f'{algorithm} Credential={endpoint["access_key_id"]}/{credential_scope}, ' +
+            f'SignedHeaders={signed_headers}, Signature=' + signature()
+        ),
+    }
