97e48cff3e9dd54b2eb98e247bfea409d3b98876
Michal Charemza <michal@charemza.name>
2018-08-11 20:11:18 +0100

    (tests) Support larger amount of output without blocking

--- core/app/tests_process.py
+++ core/app/tests_process.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-from subprocess import Popen, PIPE
 import sys
 import unittest
 from unittest.mock import Mock
@@ -28,15 +27,14 @@ class TestProcess(unittest.TestCase):
         loop = asyncio.get_event_loop()
         self.addCleanup(loop.run_until_complete, coroutine())
 
-    async def terminate_with_output(self, server_out, server_inc):
+    async def terminate(self, server_out, server_inc):
         await asyncio.sleep(1)
         server_out.terminate()
         server_inc.terminate()
+        await server_out.wait()
+        await server_inc.wait()
         # PaaS has 10 seconds to exit cleanly
         await asyncio.sleep(1)
-        output_out, _ = server_out.communicate()
-        output_inc, _ = server_inc.communicate()
-        return output_out, output_inc
 
     async def setup_manual(self, common_env):
         await delete_all_es_data()
@@ -51,26 +49,56 @@ class TestProcess(unittest.TestCase):
             'FEEDS__2__TYPE': 'activity_stream',
         }
         feed_runner_1 = await run_feed_application(read_file, lambda: 200, Mock(), 8081)
-        feed_runner_2 = Popen([sys.executable, '-m', 'verification_feed.app'], env={
-            **env,
-            'PORT': '8082',
-        }, stdout=PIPE)
-        server_out = Popen([sys.executable, '-m', 'core.app.app_outgoing'], env=env, stdout=PIPE)
-        server_inc = Popen([sys.executable, '-m', 'core.app.app_incoming'], env=env, stdout=PIPE)
+        feed_runner_2 = await asyncio.create_subprocess_exec(
+            *[sys.executable, '-m', 'verification_feed.app'], env={
+                **env,
+                'PORT': '8082',
+            }, stdout=asyncio.subprocess.PIPE)
+        server_out = await asyncio.create_subprocess_exec(
+            *[sys.executable, '-m', 'core.app.app_outgoing'],
+            env=env, stdout=asyncio.subprocess.PIPE)
+        server_inc = await asyncio.create_subprocess_exec(
+            *[sys.executable, '-m', 'core.app.app_incoming'],
+            env=env, stdout=asyncio.subprocess.PIPE)
+
+        stdout_out = [None]
+        stdout_inc = [None]
+        is_running = True
+
+        async def populate_stdout(process, stdout):
+            while is_running:
+                output = await process.stdout.readline()
+                if output != b'':
+                    stdout[0] = output
+                await asyncio.sleep(0)
+
+        asyncio.get_event_loop().create_task(populate_stdout(server_out, stdout_out))
+        asyncio.get_event_loop().create_task(populate_stdout(server_inc, stdout_inc))
 
         async def tear_down():
-            server_inc.terminate()
-            server_out.terminate()
+            nonlocal is_running
+            is_running = False
+            await asyncio.sleep(0.2)
+            if server_out.returncode is None:
+                server_out.kill()
+            if server_inc.returncode is None:
+                server_inc.kill()
             feed_runner_2.terminate()
             await feed_runner_1.cleanup()
             await asyncio.sleep(1)
 
+        def get_server_out_stdout():
+            return stdout_out[0]
+
+        def get_server_inc_stdout():
+            return stdout_inc[0]
+
         self.add_async_cleanup(tear_down)
-        return server_out, server_inc
+        return (server_out, get_server_out_stdout), (server_inc, get_server_inc_stdout)
 
     @async_test
     async def test_http_and_exit_clean(self):
-        server_out, server_inc = await self.setup_manual(mock_env())
+        (server_out, stdout_out), (server_inc, stdout_inc) = await self.setup_manual(mock_env())
         self.assertTrue(await is_http_accepted_eventually())
         await wait_until_get_working()
 
@@ -81,26 +109,26 @@ class TestProcess(unittest.TestCase):
         ids = [item['id'] for item in result['orderedItems']]
         self.assertIn('dit:activityStreamVerificationFeed:Verifier', str(ids))
 
-        output_out, output_inc = await self.terminate_with_output(server_out, server_inc)
+        await self.terminate(server_out, server_inc)
 
         final_string = b'Reached end of main. Exiting now.\n'
-        self.assertEqual(final_string, output_inc[-len(final_string):])
-        self.assertEqual(final_string, output_out[-len(final_string):])
+        self.assertEqual(final_string, stdout_inc())
+        self.assertEqual(final_string, stdout_out())
         self.assertEqual(server_inc.returncode, 0)
         self.assertEqual(server_out.returncode, 0)
 
     @async_test
     async def test_if_es_down_exit_clean(self):
-        server_out, server_inc = await self.setup_manual({
+        (server_out, stdout_out), (server_inc, stdout_inc) = await self.setup_manual({
             **mock_env(), 'ELASTICSEARCH__PORT': '9202'  # Nothing listening
         })
         self.assertTrue(await is_http_accepted_eventually())
 
-        output_out, output_inc = await self.terminate_with_output(server_out, server_inc)
+        await self.terminate(server_out, server_inc)
 
         final_string = b'Reached end of main. Exiting now.\n'
-        self.assertEqual(final_string, output_inc[-len(final_string):])
-        self.assertEqual(final_string, output_out[-len(final_string):])
+        self.assertEqual(final_string, stdout_inc())
+        self.assertEqual(final_string, stdout_out())
         self.assertEqual(server_inc.returncode, 0)
         self.assertEqual(server_out.returncode, 0)
 
@@ -115,16 +143,16 @@ class TestProcess(unittest.TestCase):
         es_runner = await run_es_application(port=9201, override_routes=routes)
         self.add_async_cleanup(es_runner.cleanup)
 
-        server_out, server_inc = await self.setup_manual({
+        (server_out, stdout_out), (server_inc, stdout_inc) = await self.setup_manual({
             **mock_env(), 'ELASTICSEARCH__PORT': '9201'
         })
         self.assertTrue(await is_http_accepted_eventually())
 
-        output_out, output_inc = await self.terminate_with_output(server_out, server_inc)
+        await self.terminate(server_out, server_inc)
 
         final_string = b'Reached end of main. Exiting now.\n'
-        self.assertEqual(final_string, output_inc[-len(final_string):])
-        self.assertEqual(final_string, output_out[-len(final_string):])
+        self.assertEqual(final_string, stdout_inc())
+        self.assertEqual(final_string, stdout_out())
         self.assertEqual(server_inc.returncode, 0)
         self.assertEqual(server_out.returncode, 0)
 
