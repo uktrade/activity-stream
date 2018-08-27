a7c853abc754d5bdeeb928f46829697a9e3679b3
Michal Charemza <michal@charemza.name>
2018-08-27 16:57:52 +0100

    (refactor) Factor out functions to be used for incoming hawk requests

--- core/app/app_hawk.py
+++ core/app/app_hawk.py
@@ -11,25 +11,32 @@ import os
 
 def get_hawk_header(access_key_id, secret_access_key, method, host, port, path, content_type,
                     content):
-    canonical_payload = \
-        b'hawk.1.payload' + b'\n' + \
-        content_type + b'\n' + \
-        content + b'\n'
-    payload_hash = base64_digest(canonical_payload)
+    payload_hash = get_payload_hash(content_type, content)
 
     timestamp = str(int(datetime.now().timestamp()))
     nonce = b64encode(os.urandom(5)).decode('utf-8')[:6]
-
-    canonical_request = \
-        f'hawk.1.header\n{timestamp}\n{nonce}\n{method}\n{path}\n{host}\n{port}\n' \
-        f'{payload_hash}\n\n'
-    mac = base64_mac(secret_access_key.encode('utf-8'), canonical_request.encode('utf-8'))
+    mac = get_mac(secret_access_key, timestamp, nonce, method, path, host, port, payload_hash)
 
     header = \
         f'Hawk mac="{mac}", hash="{payload_hash}", id="{access_key_id}", ts="{timestamp}", ' \
         f'nonce="{nonce}"'
 
     return header
+
+
+def get_payload_hash(content_type, content):
+    canonical_payload = \
+        b'hawk.1.payload' + b'\n' + \
+        content_type + b'\n' + \
+        content + b'\n'
+    return base64_digest(canonical_payload)
+
+
+def get_mac(secret_access_key, timestamp, nonce, method, path, host, port, payload_hash):
+    canonical_request = \
+        f'hawk.1.header\n{timestamp}\n{nonce}\n{method}\n{path}\n{host}\n{port}\n' \
+        f'{payload_hash}\n\n'
+    return base64_mac(secret_access_key.encode('utf-8'), canonical_request.encode('utf-8'))
 
 
 def base64_digest(data):
