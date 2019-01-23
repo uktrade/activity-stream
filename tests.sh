aa39f56107d659bc504524d539b181203b776020
alexandros giamas <agiamas@gmail.com>
2019-01-23 05:46:32 +0000

    (chore) workaround to get virtualenv working

--- tests.sh
+++ tests.sh
@@ -5,7 +5,7 @@ wget --waitretry=1 --retry-connrefused -O-  http://127.0.0.1:9200/ &> /dev/null
 echo "Elasticsearch is running"
 
 # So we have coverage for sub-processes
-SITE_PACKAGES_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
+SITE_PACKAGES_DIR=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 echo "import coverage; coverage.process_startup()" > "${SITE_PACKAGES_DIR}/coverage.pth"
 export COVERAGE_PROCESS_START=.coveragerc
 cp -r -f shared core
