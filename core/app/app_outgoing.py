f8c2d24f5d3fcfefc089237f18efba5d1b0f59d6
Michal Charemza <michal@charemza.name>
2019-03-21 09:17:52 +0000

    (refactor) Towards single place to make HTTP requests, so can add metrics

--- core/app/app_outgoing.py
+++ core/app/app_outgoing.py
@@ -264,8 +264,9 @@ async def ingest_feed_page(context, ingest_type, feed_lock, feed,
 @http_429_retry_after
 async def get_feed_contents(context, href, headers, **_):
     async with context.session.request('GET', href, data=b'', headers=headers) as result:
+        result_bytes = await result.read()
         result.raise_for_status()
-        return await result.read()
+        return result_bytes
 
 
 async def create_metrics_application(parent_context, metrics_registry, feed_endpoints):
