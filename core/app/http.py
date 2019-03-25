625c5e99155538da987fd6321422fa92d1d84480
Michal Charemza <michal@charemza.name>
2019-03-25 08:59:30 +0000

    (refactor) aiohttp parses URL, so don't do it twice

--- core/app/http.py
+++ core/app/http.py
@@ -7,16 +7,16 @@ from .metrics import (
 
 
 async def http_make_request(session, metrics, method, url, data, headers):
-    host = yarl.URL(url).host
+    parsed_url = yarl.URL(url)
 
-    with metric_timer(metrics['http_request_duration_seconds'], [host]):
-        async with session.request(method, url, data=data, headers=headers) as result:
+    with metric_timer(metrics['http_request_duration_seconds'], [parsed_url.host]):
+        async with session.request(method, parsed_url, data=data, headers=headers) as result:
             # We must read the body before the connection is closed, which can
             # be on exit of the context manager
             await result.read()
 
             with metric_counter(metrics['http_request_completed_total'],
-                                [host, str(result.status)], 1):
+                                [parsed_url.host, str(result.status)], 1):
                 # The counter is a context manager, but in this case we have nothing
                 # to wrap, we just want to increment the counter
                 pass
