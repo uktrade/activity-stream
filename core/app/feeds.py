2df9d26c40df02bdd02f590c467d93a483a50098
Michal Charemza <michal@charemza.name>
2020-11-02 08:52:35 +0000

    feat: remove Aventri
    One of its tests is very flakey. We could fix that... or, since we are
    now no longer using this in production, we choose instead to remove the
    feature.

--- core/app/feeds.py
+++ core/app/feeds.py
@@ -23,7 +23,6 @@ from .metrics import (
 )
 from .utils import (
     json_loads,
-    json_dumps,
     sub_dict_lower,
 )
 from .utils import (
@@ -35,7 +34,6 @@ def parse_feed_config(feed_config):
     by_feed_type = {
         'activity_stream': ActivityStreamFeed,
         'zendesk': ZendeskFeed,
-        'aventri': EventFeed,
         'maxemail': MaxemailFeed,
     }
     return by_feed_type[feed_config['TYPE']].parse_config(feed_config)
@@ -252,192 +250,6 @@ class ZendeskFeed(Feed):
             for ticket in feed['tickets']
             for company_number in company_numbers(ticket['description'])
         ]
-
-
-class EventFeed(Feed):
-
-    down_grace_period = 60 * 60 * 4
-
-    full_ingest_page_interval = 3
-    updates_page_interval = 60 * 60 * 24 * 30
-    exception_intervals = [120, 180, 240, 300]
-
-    @classmethod
-    def parse_config(cls, config):
-        return cls(**sub_dict_lower(
-            config, ['UNIQUE_ID', 'SEED', 'ACCOUNT_ID', 'API_KEY', 'AUTH_URL', 'EVENT_URL',
-                     'WHITELISTED_FOLDERS', 'GETADDRESS_API_KEY', 'GETADDRESS_API_URL'
-                     ]))
-
-    def __init__(self, unique_id, seed, account_id, api_key,
-                 auth_url, event_url, whitelisted_folders, getaddress_api_key, getaddress_api_url):
-        self.lock = asyncio.Lock()
-        self.unique_id = unique_id
-        self.seed = seed
-        self.account_id = account_id
-        self.api_key = api_key
-        self.auth_url = auth_url
-        self.event_url = event_url
-        self.getaddress_api_key = getaddress_api_key
-        self.getaddress_api_url = getaddress_api_url
-        self.accesstoken = None
-        self.whitelisted_folders = whitelisted_folders
-
-    @staticmethod
-    def next_href(_):
-        """ aventri API does not support pagination
-            returns (None)
-        """
-        return None
-
-    async def auth_headers(self, context, __):
-        result = await http_make_request(
-            context.session, context.metrics, 'POST', self.auth_url, data={
-                'accountid': self.account_id, 'key': self.api_key,
-            }, headers={})
-        result.raise_for_status()
-
-        self.accesstoken = json_loads(result._body)['accesstoken']
-        return {
-            'accesstoken': self.accesstoken,
-        }
-
-    async def get_activities(self, context, feed):
-        async def get_event(event_id):
-            event_lookup = await context.redis_client.execute('GET', f'event-{event_id}')
-            if event_lookup:
-                try:
-                    return json_loads(event_lookup.decode('utf-8'))
-                except UnicodeDecodeError:
-                    await context.redis_client.execute('DEL', f'event-{event_id}')
-                    return await fetch_from_aventri(event_id)
-            else:
-                return await fetch_from_aventri(event_id)
-
-        def can_get_location(event):
-            return event.get('location') and event['location'].get('postcode')
-
-        async def fetch_from_aventri(event_id):
-            url = self.event_url.format(event_id=event_id)
-
-            with logged(context.logger.debug, context.logger.warning,
-                        'Fetching event (%s)', [url]):
-                result = await http_make_request(
-                    context.session, context.metrics, 'GET', url, data=b'',
-                    headers={'accesstoken': self.accesstoken})
-                result.raise_for_status()
-
-            event = json_loads(result._body)
-            if can_get_location(event):
-                event = await get_location(event)
-
-            await context.redis_client.execute(
-                'SETEX', f'event-{event_id}', 60*60*24*7, json_dumps(event))
-            return event
-
-        async def get_location(event):
-            postcode = event['location']['postcode']
-
-            # Search Reddis first
-            redis_lookup = await context.redis_client.execute('GET', f'address-{postcode}')
-            if redis_lookup:
-                split_lat_lng = redis_lookup.decode('utf-8').split(',')
-                event['geocoordinates'] = {}
-                event['geocoordinates']['lat'] = split_lat_lng[0]
-                event['geocoordinates']['lon'] = split_lat_lng[1]
-            else:  # Try AddressLookup API
-                event = await fetch_address_from_getaddressio(event, postcode)
-            return event
-
-        async def fetch_address_from_getaddressio(event, postcode):
-            url = self.getaddress_api_url + \
-                f'/find/{postcode}?api-key={self.getaddress_api_key}'
-            resp = await http_make_request(context.session, context.metrics, 'GET', url,
-                                           data=b'', headers={})
-            if resp.status == 200:
-                geo_result = json_loads(await resp.text())
-                if geo_result.get('latitude'):
-                    event['geocoordinates'] = {}
-                    event['geocoordinates']['lat'] = str(
-                        geo_result['latitude'])
-                    event['geocoordinates']['lon'] = str(
-                        geo_result['longitude'])
-                    joined_lat_lng = ','.join(
-                        [str(geo_result['latitude']),
-                         str(geo_result['longitude'])]
-                    ).encode('utf-8')
-                    await context.redis_client.execute(
-                        'SET', f'address-{postcode}', joined_lat_lng)
-            return event
-
-        now = datetime.datetime.now().isoformat()
-        return [
-            {
-                'id': 'dit:aventri:Event:' + str(event['eventid']) + ':Create',
-                'type': 'Search',
-                'eventid': event['eventid'],
-                'dit:application': 'aventri',
-                'object': {
-                    'type': ['Event', 'dit:aventri:Event'],
-                    'id': 'dit:aventri:Event:' + event['eventid'],
-                    'name': event['name'],
-                    'url': event['url'],
-                    'content': event['description'],
-                    'startdate': event['startdate'],
-                    'enddate': event['enddate'],
-                    'foldername': event['foldername'],
-                    'location': event['location'],
-                    'geocoordinates': event.get('geocoordinates'),
-                    'language': event['defaultlanguage'],
-                    'timezone': event['timezone'],
-                    'currency': event['standardcurrency'],
-                    'price_type': event['price_type'],
-                    'price': event['pricepoints'],
-                    'published': now,
-                }
-            }
-            for page_event in feed
-            for event in [await get_event(page_event['eventid'])]
-            if self.should_include(context, event)
-        ]
-
-    def should_include(self, context, event):
-        # event must be not deleted
-        # startdate should be >= today and not null
-        # enddate should be >= startdate and not null
-        # folderid or foldername should be != internal events folder
-        # name, url, description should be not null
-
-        allowed_folders = self.whitelisted_folders.split(',')
-        now = datetime.datetime.today().strftime('%Y-%m-%d')
-        try:
-            should_include = (
-                event['eventid'] is not None and
-                event['deleted'] != 0 and
-                event['enddate'] >= event['startdate'] >= now and
-                event['name'] is not None and
-                event['url'] is not None and
-                event['description'] is not None and
-                event['include_calendar'] == '1' and
-                event['status'] == 'Live' and
-                event['foldername'] in allowed_folders
-            )
-
-        except KeyError:
-            should_include = False
-
-        loggable_event = {
-            key: event[key]
-            for key in (
-                'eventid', 'deleted', 'enddate', 'startdate', 'name', 'url', 'description',
-                'include_calendar', 'status', 'foldername',
-            )
-            if key in event
-        }
-        context.logger.debug('Event data: (%s) should_include: (%s)',
-                             loggable_event, should_include)
-
-        return should_include
 
 
 class MaxemailFeed(Feed):
