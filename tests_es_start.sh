b716cc62b5a89d4d486d614f3c37b4ec14d4d69e
Michal Charemza <michal@charemza.name>
2022-02-16 15:05:25 +0000

    feat: move to OpenSearch

--- tests_es_start.sh
+++ tests_es_start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -xe
 
-docker run --rm --name activity-stream-elasticsearch -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.9.1
+docker run --rm --name activity-stream-elasticsearch -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" \
+    --entrypoint bash \
+    opensearchproject/opensearch:1.1.0 \
+    -c 'rm -r -f /usr/share/opensearch/plugins/opensearch-security && exec /usr/share/opensearch/opensearch-docker-entrypoint.sh'
