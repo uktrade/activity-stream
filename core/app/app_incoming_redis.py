e94b1ae5b4992103387eb3c5a2f49ae7b8aada1b
Nick Ross <nick.ir.ross@gmail.com>
2023-11-14 16:13:30 +0000

    wip: redis upgrades

--- core/app/app_incoming_redis.py
+++ core/app/app_incoming_redis.py
@@ -1,13 +1,13 @@
 async def redis_get_metrics(context):
-    return await context.redis_client.execute('GET', 'metrics')
+    return await context.redis_client.execute_command('GET', 'metrics')
 
 
 async def set_nonce_nx(context, nonce_key, nonce_expire):
-    return await context.redis_client.execute('SET', nonce_key, '1',
-                                              'EX', nonce_expire, 'NX')
+    return await context.redis_client.execute_command('SET', nonce_key, '1',
+                                                      'EX', nonce_expire, 'NX')
 
 
 async def get_feeds_status(context, feed_ids):
-    return await context.redis_client.execute('MGET', *[
+    return await context.redis_client.execute_command('MGET', *[
         feed_id + '-status' for feed_id in feed_ids
     ])
