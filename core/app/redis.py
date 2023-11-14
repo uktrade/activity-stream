2366265917ef38acadd8768f992975b398a03a3c
Nick Ross <nick.ir.ross@gmail.com>
2023-11-14 16:47:15 +0000

    wip: redis upgrades

--- core/app/redis.py
+++ core/app/redis.py
@@ -3,4 +3,4 @@ import aioredis
 
 async def redis_get_client(redis_uri):
     pool = aioredis.BlockingConnectionPool.from_url(redis_uri, max_connections=3)
-    return aioredis.Redis(connection_pool=pool).client()
+    return aioredis.Redis(connection_pool=pool)
