50b49fd8e498cf5d1a2bd9e9343769c9b9ec0040
Nick Ross <nick.ir.ross@gmail.com>
2023-11-14 16:00:07 +0000

    wip: redis upgrades

--- core/app/redis.py
+++ core/app/redis.py
@@ -2,10 +2,11 @@ import aioredis
 
 
 async def redis_get_client(redis_uri):
-    return aioredis.from_url(
+    redis = aioredis.from_url(
         redis_uri,
         min_connections=3,
         max_connections=3,
         encoding='utf-8',
         decode_responses=True
     )
+    return redis.client()
