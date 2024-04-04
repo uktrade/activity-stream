cce23e93e2b2201841e7987c971b8172ff3f7d56
Christopher <christopher.sunkel@digital.trade.gov.uk>
2024-04-04 16:19:52 +0100

    Use Opensearch 2.11.0

--- tests_es_start.sh
+++ tests_es_start.sh
@@ -2,5 +2,5 @@
 
 docker run --rm --name activity-stream-elasticsearch -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" \
     --entrypoint bash \
-    opensearchproject/opensearch:1.1.0 \
+    opensearchproject/opensearch:2.11.0 \
     -c 'rm -r -f /usr/share/opensearch/plugins/opensearch-security && exec /usr/share/opensearch/opensearch-docker-entrypoint.sh'
